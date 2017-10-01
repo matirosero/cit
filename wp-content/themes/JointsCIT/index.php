ef92c495a47cb7f76c4bd41e5ab5de877f7e2171
MatiRosero <gekidasa@gmail.com>
2017-09-30 22:13:38 -0600

    sass partials

--- wp-content/themes/JointsCIT/index.php
+++ wp-content/themes/JointsCIT/index.php
@@ -1,32 +1,40 @@
 <?php get_header(); ?>
-			
-	<div id="content">
-	
+
+<div class="row expand">
+
+	<div id="side-menu" class="large-2 columns">
+		Side menu hi
+	</div>
+
+	<div id="content" class="large-10 columns">
+
 		<div id="inner-content" class="row">
-	
+
 		    <main id="main" class="large-8 medium-8 columns" role="main">
-		    
+
 			    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			 
+
 					<!-- To see additional archive styles, visit the /parts directory -->
 					<?php get_template_part( 'parts/loop', 'archive' ); ?>
-				    
-				<?php endwhile; ?>	
+
+				<?php endwhile; ?>
 
 					<?php joints_page_navi(); ?>
-					
+
 				<?php else : ?>
-											
+
 					<?php get_template_part( 'parts/content', 'missing' ); ?>
-						
+
 				<?php endif; ?>
-																								
+
 		    </main> <!-- end #main -->
-		    
+
 		    <?php get_sidebar(); ?>
 
 		</div> <!-- end #inner-content -->
 
 	</div> <!-- end #content -->
+
+</div><!-- .row -->
 
 <?php get_footer(); ?>
