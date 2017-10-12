f4a031a223c73278800ef9c1602db07335f279b4
MatiRosero <gekidasa@gmail.com>
2017-10-12 15:49:07 -0600

    sidebar style, localize readmore

--- wp-content/themes/JointsCIT/parts/content-byline.php
+++ wp-content/themes/JointsCIT/parts/content-byline.php
@@ -1,5 +1,5 @@
 <p class="byline">
-	Posted by <?php if ( function_exists( 'coauthors_posts_links' ) ) {
+	<?php _e( 'Posted by', 'jointswp' );  ?> <?php if ( function_exists( 'coauthors_posts_links' ) ) {
 	    coauthors_posts_links();
 	} else {
 	    the_author_posts_link();
