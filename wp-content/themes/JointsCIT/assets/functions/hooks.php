869bb2a9e730299229615e8ae40b9bf417bb899d
MatiRosero <gekidasa@gmail.com>
2017-10-12 00:50:11 -0600

    first post bigger

--- wp-content/themes/JointsCIT/assets/functions/hooks.php
+++ wp-content/themes/JointsCIT/assets/functions/hooks.php
@@ -7,7 +7,7 @@ add_filter('excerpt_more', 'new_excerpt_more', 21 );
 
 function the_excerpt_more_link( $excerpt ){
     $post = get_post();
-    $excerpt .= ' <a class="button round" href="'. get_permalink($post->ID) . '">Read more</a>';
+    $excerpt .= '<p class="read-more"><a class="button round" href="'. get_permalink($post->ID) . '">Read more</a></p>';
     return $excerpt;
 }
 add_filter( 'the_excerpt', 'the_excerpt_more_link', 21 );
