bfed264ac396231459438d2acdb09b1fa04307cb
MatiRosero <gekidasa@gmail.com>
2017-11-03 16:26:06 -0600

    button bg gradient and orange anchor hover

--- wp-content/themes/JointsCIT/parts/content-byline.php
+++ wp-content/themes/JointsCIT/parts/content-byline.php
@@ -1,5 +1,5 @@
 <p class="byline">
-	<?php _e( 'Posted by', 'jointswp' );  ?> <?php if ( function_exists( 'coauthors_posts_links' ) ) {
+	<?php _e( 'By', 'jointswp' );  ?> <?php if ( function_exists( 'coauthors_posts_links' ) ) {
 	    coauthors_posts_links();
 	} else {
 	    the_author_posts_link();
