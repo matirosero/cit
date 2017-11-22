fb1c5a18e1cb925dc95bdfc52c1cacdb6b18d9d2
MatiRosero <gekidasa@gmail.com>
2017-11-22 15:48:10 -0600

    searchform.php uses field group, topbar search has no space between input and button

--- wp-content/themes/JointsCIT/searchform.php
+++ wp-content/themes/JointsCIT/searchform.php
@@ -1,7 +1,11 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-	<label>
-		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'jointswp' ) ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search...', 'jointswp' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'jointswp' ) ?>" />
-	</label>
-	<input type="submit" class="search-submit button" value="<?php echo esc_attr_x( 'Search', 'jointswp' ) ?>" />
+	<div class="input-group">
+		<label>
+			<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'jointswp' ) ?></span>
+			<input type="search" class="search-field input-group-field" placeholder="<?php echo esc_attr_x( 'Search...', 'jointswp' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'jointswp' ) ?>" />
+		</label>
+		<div class="input-group-button">
+			<input type="submit" class="search-submit button" value="<?php echo esc_attr_x( 'Search', 'jointswp' ) ?>" />
+		</div>
+	</div>
 </form>
