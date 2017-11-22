48fb99312a6178b3ea2ded78d11c0e8ed904e086
MatiRosero <gekidasa@gmail.com>
2017-11-22 15:27:40 -0600

    topbar search padding

--- wp-content/themes/JointsCIT/parts/nav-offcanvas-topbar.php
+++ wp-content/themes/JointsCIT/parts/nav-offcanvas-topbar.php
@@ -18,7 +18,9 @@
 					<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'jointswp' ) ?></span>
 					<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search...', 'jointswp' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'jointswp' ) ?>" />
 				</label></li>
-				<li><input type="submit" class="search-submit button" value="<?php echo esc_attr_x( 'Search', 'jointswp' ) ?>" /></li>
+				<li>
+					<input type="submit" class="search-submit button" value="<?php echo esc_attr_x( 'Search', 'jointswp' ) ?>" />
+				<span class="sb-icon-search"></span></li>
 			</ul>
 		</form>
 
