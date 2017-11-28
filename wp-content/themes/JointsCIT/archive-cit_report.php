b725574f4d57e3bfd8fac0ba66d4acaa53af7bd8
MatiRosero <gekidasa@gmail.com>
2017-11-27 22:31:22 -0600

    reports back to default archive

--- wp-content/themes/JointsCIT/archive-cit_report.php
+++ wp-content/themes/JointsCIT/archive-cit_report.php
@@ -13,7 +13,7 @@
 			    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 					<!-- To see additional archive styles, visit the /parts directory -->
-					<?php get_template_part( 'parts/loop', 'archive-minimal' ); ?>
+					<?php get_template_part( 'parts/loop', 'archive' ); ?>
 
 				<?php endwhile; ?>
 
