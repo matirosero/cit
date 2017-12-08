ff16d7a3f1491a23b49faed61bcdf7a6a179fab2
MatiRosero <gekidasa@gmail.com>
2017-12-08 14:47:53 -0600

    post type label

--- wp-content/themes/JointsCIT/parts/loop-archive.php
+++ wp-content/themes/JointsCIT/parts/loop-archive.php
@@ -1,15 +1,18 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article">
 	<header class="article-header">
-<?php 
-if ( is_search() ) : 
-	$post_type_object = get_post_type_object( get_post_type() );
-	$post_type = esc_html( $post_type_object->labels->singular_name );
-	if ( $post_type == 'Entrada' ) :
-		$post_type = 'Artículo';
-	endif;
-	?>
-	<span class="post-type secondary label"><?php echo $post_type; ?></span>
-<?php endif; ?>
+		
+		<?php 
+		//Show post type if search
+		if ( is_search() ) : 
+			$post_type_object = get_post_type_object( get_post_type() );
+			$post_type = esc_html( $post_type_object->labels->singular_name );
+			if ( $post_type == 'Entrada' ) :
+				$post_type = 'Artículo';
+			endif;
+			?>
+			<p class="post-type secondary label"><?php echo $post_type; ?></p>
+		<?php endif; ?>
+
 		<h2 class="entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 		<?php 
 		if ( is_singular('cit_report') || is_post_type_archive('cit_report') ) : 
