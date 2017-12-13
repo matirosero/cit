16d4e6b3f9c72a39904c06436b50ed9082f330d8
MatiRosero <gekidasa@gmail.com>
2017-12-13 15:10:57 -0600

    override login

--- wp-content/themes/JointsCIT/functions.php
+++ wp-content/themes/JointsCIT/functions.php
@@ -53,7 +53,7 @@ require_once(get_template_directory().'/assets/functions/csv-helpers.php');
 // require_once(get_template_directory().'/assets/functions/custom-post-type.php');
 
 // Customize the WordPress login menu
-// require_once(get_template_directory().'/assets/functions/login.php'); 
+require_once(get_template_directory().'/assets/functions/login.php'); 
 
 // Customize the WordPress admin
 // require_once(get_template_directory().'/assets/functions/admin.php'); 
