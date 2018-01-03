7648ff42589b9e99627900a7474b9709a46f4a04
MatiRosero <gekidasa@gmail.com>
2018-01-03 13:37:34 -0600

    start mobile topbar changes
    moed logo to left, unstacked menu for small, created tablet menu

--- wp-content/themes/JointsCIT/assets/functions/menu.php
+++ wp-content/themes/JointsCIT/assets/functions/menu.php
@@ -3,6 +3,7 @@
 register_nav_menus(
 	array(
 		'main-nav' => __( 'The Main Menu', 'jointswp' ),   // Main nav in header
+        'main-tablet-nav' => __( 'The Main Menu for Tablets', 'jointswp' ),   // Main nav in header
         'sidebar-nav' => __( 'The Sidebar Menu', 'jointswp' ),   // Main nav in header
         'mobile-nav' => __( 'The Mobile/Offcanvas Menu', 'jointswp' ),   // Main nav in header
 		'footer-links' => __( 'Footer Links', 'jointswp' ) // Secondary nav in footer
