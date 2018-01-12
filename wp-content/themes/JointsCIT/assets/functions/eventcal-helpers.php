1b8b4e38e1d17c7133e21e021f20d95f623c907b
MatiRosero <gekidasa@gmail.com>
2018-01-11 20:30:14 -0600

    changed enterprise to empresarial

--- wp-content/themes/JointsCIT/assets/functions/eventcal-helpers.php
+++ wp-content/themes/JointsCIT/assets/functions/eventcal-helpers.php
@@ -176,7 +176,7 @@ function mro_cit_rsvp_form() {
 		elseif ( current_user_can( 'buy_event_tickets' ) ) :
 			echo '<p class="callout primary small">Los Afiliados Personales tienen la posibilidad de adquirir entradas a los eventos del Club. Llene este formulario y nos comunicaremos con más detalles.</p>'
 				.do_shortcode( '[caldera_form id="CF5a222f8dca7c7"]' );
-		elseif ( members_current_user_has_role( 'afiliado_enterprise_pendiente' ) ) :
+		elseif ( members_current_user_has_role( 'afiliado_empresarial_pendiente' ) ) :
 			echo '<h3>Adquiera entradas al evento</h3>';
 			echo '<p class="callout warning small">Aún está pendiente finalizar su afiliación. Comuníquese con Leda Mora, teléfono 2223-5923, fax 2223-1997, correo <a href="mailto:leda@clubdeinvestigacion.com">leda@clubdeinvestigacion.com</a> si desea asistir al evento.</p>';
 		else:
