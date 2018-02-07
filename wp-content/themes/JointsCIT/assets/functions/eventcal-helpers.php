367b20e601a8b9a82891c8c9a1e1ea3a6f873f1d
MatiRosero <gekidasa@gmail.com>
2018-02-07 10:36:57 -0600

    rsvp as person

--- wp-content/themes/JointsCIT/assets/functions/eventcal-helpers.php
+++ wp-content/themes/JointsCIT/assets/functions/eventcal-helpers.php
@@ -171,12 +171,22 @@ function mro_cit_rsvp_form() {
 	    endif;
 
 	    if ( current_user_can( 'rsvp_event' ) ) :
-		    echo '<p>Llene este formulario, o comuníquese con Leda Mora, teléfono 2223-5923, fax 2223-1997, correo <a href="mailto:leda@clubdeinvestigacion.com">leda@clubdeinvestigacion.com</a></p>'
-		    	.do_shortcode( '[caldera_form id="CF5a1708a8ae022"]' );
+		    echo '<p>Llene este formulario, o comuníquese con Leda Mora, teléfono 2223-5923, fax 2223-1997, correo <a href="mailto:leda@clubdeinvestigacion.com">leda@clubdeinvestigacion.com</a></p>';
+
+		    if ( members_current_user_has_role( 'afiliado_empresarial' ) || members_current_user_has_role( 'afiliado_institucional' ) ) :
+
+		    	echo do_shortcode( '[caldera_form id="CF5a1708a8ae022"]' );
+
+		    else: 
+
+		    	echo do_shortcode( '[caldera_form id="CF5a7b293be0d36"]' );
+
+		    endif;
+
 		elseif ( current_user_can( 'buy_event_tickets' ) ) :
 			echo '<p class="callout primary small">Los Afiliados Personales tienen la posibilidad de adquirir entradas a los eventos del Club. Llene este formulario y nos comunicaremos con más detalles.</p>'
 				.do_shortcode( '[caldera_form id="CF5a222f8dca7c7"]' );
-		elseif ( members_current_user_has_role( 'afiliado_empresarial_pendiente' ) ) :
+		elseif ( members_current_user_has_role( 'afiliado_empresarial_pendiente' ) || members_current_user_has_role( 'afiliado_institucional_pendiente' ) ) :
 			echo '<h3>Adquiera entradas al evento</h3>';
 			echo '<p class="callout warning small">Aún está pendiente finalizar su afiliación. Comuníquese con Leda Mora, teléfono 2223-5923, fax 2223-1997, correo <a href="mailto:leda@clubdeinvestigacion.com">leda@clubdeinvestigacion.com</a> si desea asistir al evento.</p>';
 		else:
