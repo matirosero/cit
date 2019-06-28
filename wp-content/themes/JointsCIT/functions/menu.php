f7453c24dcff502a6d20e8ec030266e4f356bca2
MatRosero <hello@matilderosero.com>
2019-06-28 12:30:21 -0600

    changed functions/menu to mine

--- wp-content/themes/JointsCIT/functions/menu.php
+++ wp-content/themes/JointsCIT/functions/menu.php
@@ -2,78 +2,126 @@
 // Register menus
 register_nav_menus(
 	array(
-		'main-nav'		=> __( 'The Main Menu', 'jointswp' ),		// Main nav in header
-		'offcanvas-nav'	=> __( 'The Off-Canvas Menu', 'jointswp' ),	// Off-Canvas nav
-		'footer-links'	=> __( 'Footer Links', 'jointswp' )			// Secondary nav in footer
+		'main-nav' => __( 'The Main Menu', 'jointswp' ),   // Main nav in header
+        'main-tablet-nav' => __( 'The Main Menu for Tablets', 'jointswp' ),   // Main nav in header
+        'sidebar-nav' => __( 'The Sidebar Menu', 'jointswp' ),   // Main nav in header
+        'mobile-nav' => __( 'The Mobile/Offcanvas Menu', 'jointswp' ),   // Main nav in header
+		'footer-links' => __( 'Footer Links', 'jointswp' ) // Secondary nav in footer
 	)
 );
 
+
+// The Top Menu
+function joints_accordion_nav() {
+     wp_nav_menu(array(
+        'container' => false,                           // Remove nav container
+        'menu_class' => 'vertical menu accordion-menu',       // Adding custom nav class
+        'items_wrap' => '<ul id="%1$s" class="%2$s" data-accordion-menu>%3$s</ul>',
+        'theme_location' => 'sidebar-nav',                  // Where it's located in the theme
+        'depth' => 5,                                   // Limit the depth of the nav
+        'fallback_cb' => false,                         // Fallback function (see below)
+        'walker' => new Accordion_Menu_Walker()
+    ));
+}
+
+
+// Big thanks to Brett Mason (https://github.com/brettsmason) for the awesome walker
+class Accordion_Menu_Walker extends Walker_Nav_Menu {
+    function start_lvl(&$output, $depth = 0, $args = Array() ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "\n$indent<ul class=\"menu vertical nested\">\n";
+    }
+}
+
+
 // The Top Menu
 function joints_top_nav() {
-	wp_nav_menu(array(
-		'container'			=> false,						// Remove nav container
-		'menu_id'			=> 'main-nav',					// Adding custom nav id
-		'menu_class'		=> 'medium-horizontal menu',	// Adding custom nav class
-		'items_wrap'		=> '<ul id="%1$s" class="%2$s" data-responsive-menu="accordion medium-dropdown">%3$s</ul>',
-		'theme_location'	=> 'main-nav',					// Where it's located in the theme
-		'depth'				=> 5,							// Limit the depth of the nav
-		'fallback_cb'		=> false,						// Fallback function (see below)
-		'walker'			=> new Topbar_Menu_Walker()
-	));
+	 wp_nav_menu(array(
+        'container' => false,                           // Remove nav container
+        'menu_class' => 'vertical medium-horizontal menu',       // Adding custom nav class
+        'items_wrap' => '<ul id="%1$s" class="%2$s" data-responsive-menu="accordion medium-dropdown" data-close-on-click-inside="false">%3$s</ul>',
+        'theme_location' => 'main-nav',        			// Where it's located in the theme
+        'depth' => 5,                                   // Limit the depth of the nav
+        'fallback_cb' => false,                         // Fallback function (see below)
+        'walker' => new Topbar_Menu_Walker()
+    ));
+} 
+
+
+// The Top Tablet Menu
+function joints_top_tablet_nav() {
+     wp_nav_menu(array(
+        'container' => false,                           // Remove nav container
+        'menu_class' => 'vertical medium-horizontal menu',       // Adding custom nav class
+        'items_wrap' => '<ul id="%1$s" class="%2$s" data-responsive-menu="accordion medium-dropdown" data-close-on-click-inside="false">%3$s</ul>',
+        'theme_location' => 'main-tablet-nav',                 // Where it's located in the theme
+        'depth' => 5,                                   // Limit the depth of the nav
+        'fallback_cb' => false,                         // Fallback function (see below)
+        'walker' => new Topbar_Menu_Walker()
+    ));
+} 
+
+
+add_filter( 'wp_nav_menu_items', 'mro_cit_add_loginout_link', 10, 2 );
+function mro_cit_add_loginout_link( $items, $args ) {
+    if (is_user_logged_in() && $args->theme_location == 'mobile-nav') {
+        $items .= '<li><a href="' . wp_logout_url() . '">' . __( 'Log out', 'jointswp' ) . '</a></li>';
+    }
+    return $items;
 }
+
 
 // Big thanks to Brett Mason (https://github.com/brettsmason) for the awesome walker
 class Topbar_Menu_Walker extends Walker_Nav_Menu {
-	function start_lvl(&$output, $depth = 0, $args = Array() ) {
-		$indent = str_repeat("\t", $depth);
-		$output .= "\n$indent<ul class=\"menu\">\n";
-	}
+    function start_lvl(&$output, $depth = 0, $args = Array() ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "\n$indent<ul class=\"menu\">\n";
+    }
 }
 
 // The Off Canvas Menu
 function joints_off_canvas_nav() {
-	wp_nav_menu(array(
-		'container'			=> false,							// Remove nav container
-		'menu_id'			=> 'offcanvas-nav',					// Adding custom nav id
-		'menu_class'		=> 'vertical menu accordion-menu',	// Adding custom nav class
-		'items_wrap'		=> '<ul id="%1$s" class="%2$s" data-accordion-menu>%3$s</ul>',
-		'theme_location'	=> 'offcanvas-nav',					// Where it's located in the theme
-		'depth'				=> 5,								// Limit the depth of the nav
-		'fallback_cb'		=> false,							// Fallback function (see below)
-		'walker'			=> new Off_Canvas_Menu_Walker()
-	));
-}
+	 wp_nav_menu(array(
+        'container' => false,                           // Remove nav container
+        'menu_class' => 'vertical menu',       // Adding custom nav class
+        'items_wrap' => '<ul id="%1$s" class="%2$s" data-accordion-menu>%3$s</ul>',
+        'theme_location' => 'mobile-nav',        			// Where it's located in the theme
+        'depth' => 5,                                   // Limit the depth of the nav
+        'fallback_cb' => false,                         // Fallback function (see below)
+        'walker' => new Off_Canvas_Menu_Walker()
+    ));
+} 
 
 class Off_Canvas_Menu_Walker extends Walker_Nav_Menu {
-	function start_lvl(&$output, $depth = 0, $args = Array() ) {
-		$indent = str_repeat("\t", $depth);
-		$output .= "\n$indent<ul class=\"vertical menu\">\n";
-	}
+    function start_lvl(&$output, $depth = 0, $args = Array() ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "\n$indent<ul class=\"vertical menu\">\n";
+    }
 }
 
 // The Footer Menu
 function joints_footer_links() {
-	wp_nav_menu(array(
-		'container'			=> 'false',				// Remove nav container
-		'menu_id'			=> 'footer-links',		// Adding custom nav id
-		'menu_class'		=> 'menu',				// Adding custom nav class
-		'theme_location'	=> 'footer-links',		// Where it's located in the theme
-		'depth'				=> 0,					// Limit the depth of the nav
-		'fallback_cb'		=> ''					// Fallback function
+    wp_nav_menu(array(
+    	'container' => 'false',                         // Remove nav container
+    	'menu' => __( 'Footer Links', 'jointswp' ),   	// Nav name
+    	'menu_class' => 'menu',      					// Adding custom nav class
+    	'theme_location' => 'footer-links',             // Where it's located in the theme
+        'depth' => 0,                                   // Limit the depth of the nav
+    	'fallback_cb' => ''  							// Fallback function
 	));
 } /* End Footer Menu */
 
 // Header Fallback Menu
 function joints_main_nav_fallback() {
 	wp_page_menu( array(
-		'show_home'		=> true,
-		'menu_class'	=> '',		// Adding custom nav class
-		'include'		=> '',
-		'exclude'		=> '',
-		'echo'			=> true,
-		'link_before'	=> '',		// Before each link
-		'link_after'	=> ''		// After each link
-	));
+		'show_home' => true,
+    	'menu_class' => '',      						// Adding custom nav class
+		'include'     => '',
+		'exclude'     => '',
+		'echo'        => true,
+        'link_before' => '',                           // Before each link
+        'link_after' => ''                             // After each link
+	) );
 }
 
 // Footer Fallback Menu
@@ -83,9 +131,15 @@ function joints_footer_links_fallback() {
 
 // Add Foundation active class to menu
 function required_active_nav_class( $classes, $item ) {
-	if ( $item->current == 1 || $item->current_item_ancestor == true ) {
-		$classes[] = 'active';
-	}
-	return $classes;
+    
+    $url = strpos( $item->url, '/sobre-el-club/#' );
+
+    if ( $item->url === "#" ) {
+        $classes[] = 'no-hover';
+    }
+    if ( ( $item->current == 1 || $item->current_item_ancestor == true ) && $url=== false ) {
+        $classes[] = 'active';
+    }
+    return $classes;
 }
 add_filter( 'nav_menu_css_class', 'required_active_nav_class', 10, 2 );
