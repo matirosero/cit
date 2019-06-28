3e0028cff208d44168a69451832bac1e96431dca
MatRosero <hello@matilderosero.com>
2019-06-28 12:37:28 -0600

    updated enqueue scripts

--- wp-content/themes/JointsCIT/functions/enqueue-scripts.php
+++ wp-content/themes/JointsCIT/functions/enqueue-scripts.php
@@ -12,5 +12,22 @@ function site_scripts() {
     if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
       wp_enqueue_script( 'comment-reply' );
     }
+
+    /*
+     * TEMP SCRIPTS
+     */
+    // wp_enqueue_script( 'load-tab-content-js', get_template_directory_uri() . '/assets/js/mro-load-tab-content.js', array( 'jquery' ), '', true );
+    wp_enqueue_script( 'reload-eq', get_template_directory_uri() . '/assets/js/mro-reload-eq.js', array( 'jquery' ), '', true );
+
+    wp_enqueue_script( 'toggle-fields', get_template_directory_uri() . '/assets/js/mro-toggle-fields.js', array( 'jquery' ), '', true );
+
+    /*
+     * Google Fonts
+     *
+     * font-family: 'Roboto Slab', serif; 400 700
+     * font-family: 'Libre Franklin', sans-serif; 900
+     * font-family: 'Open Sans', sans-serif; 400 700
+     */
+    // wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Libre+Franklin:900|Open+Sans:400,700|Roboto+Slab:400,700', false );
 }
 add_action('wp_enqueue_scripts', 'site_scripts', 999);
