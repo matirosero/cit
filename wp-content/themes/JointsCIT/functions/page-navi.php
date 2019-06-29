d955043fe0341b3c896085b29b8ec419a9899b1b
MatRosero <hello@matilderosero.com>
2019-06-29 01:29:33 -0600

    center pagination

--- wp-content/themes/JointsCIT/functions/page-navi.php
+++ wp-content/themes/JointsCIT/functions/page-navi.php
@@ -4,17 +4,19 @@
 		$big = 999999999; // This needs to be an unlikely integer
 		// For more options and info view the docs for paginate_links()
 		// http://codex.wordpress.org/Function_Reference/paginate_links
-		$paginate_links = paginate_links( array(
-			'base' => str_replace( $big, '%#%', html_entity_decode( get_pagenum_link( $big ) ) ),
-			'current' => max( 1, get_query_var( 'paged' ) ),
-			'total' => $wp_query->max_num_pages,
-			'mid_size' => 5,
-			'prev_next' => true,
-		    'prev_text' => __( '&laquo;', 'jointswp' ),
-		    'next_text' => __( '&raquo;', 'jointswp' ),
-			'type' => 'list',
-		) );
-		$paginate_links = str_replace( "<ul class='page-numbers'>", "<ul class='pagination'>", $paginate_links );
+		$paginate_links = paginate_links( 
+			array(
+				'base' => str_replace( $big, '%#%', html_entity_decode( get_pagenum_link( $big ) ) ),
+				'current' => max( 1, get_query_var( 'paged' ) ),
+				'total' => $wp_query->max_num_pages,
+				'mid_size' => 2,
+				'prev_next' => true,
+			    'prev_text' => __( '&laquo;', 'jointswp' ),
+			    'next_text' => __( '&raquo;', 'jointswp' ),
+				'type' => 'list',
+			) 
+		);
+		$paginate_links = str_replace( "<ul class='page-numbers'>", "<ul class='pagination text-center'>", $paginate_links );
 		$paginate_links = str_replace( '<li><span class="page-numbers dots">', "<li><a href='#'>", $paginate_links );
 		$paginate_links = str_replace( "<li><span class='page-numbers current'>", "<li class='current'>", $paginate_links );
 		$paginate_links = str_replace( '</span>', '</a>', $paginate_links );
