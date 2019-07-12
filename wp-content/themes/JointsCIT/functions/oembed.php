a1e6865a0b0ef9477356aa3abf783cf18566f328
MatRosero <hello@matilderosero.com>
2019-07-12 00:27:41 -0600

    embeds iframe with zoom video

--- wp-content/themes/JointsCIT/functions/oembed.php
+++ wp-content/themes/JointsCIT/functions/oembed.php
@@ -9,18 +9,18 @@
  */
 add_action( 'init', function() {
     wp_embed_register_handler( 
-        'forbes', 
-        '#http://www\.forbes\.com/video/([\d]+)/?#i', 
-        'wp_embed_handler_forbes' 
+        'zoom', 
+        '#https://zoom\.us/recording/share/([a-zA-Z0-9_-]+)/?#i', 
+        'wp_embed_handler_zoom' 
     );
 
 } );
 
-function wp_embed_handler_forbes( $matches, $attr, $url, $rawattr ) {
+function wp_embed_handler_zoom( $matches, $attr, $url, $rawattr ) {
     $embed = sprintf(
-        '<iframe class="forbes-video" src="https://players.brightcove.net/2097119709001/598f142b-5fda-4057-8ece-b03c43222b3f_default/index.html?videoId=%1$s" width="600" height="400" frameborder="0" scrolling="no"></iframe>',
+        '<div class="zoom-container"><iframe class="zoom-embed" src="https://zoom.us/recording/share/%1$s" width="1200" height="675" frameborder="0" scrolling="no"></iframe></div>',
         esc_attr( $matches[1] ) 
      );
 
-    return apply_filters( 'embed_forbes', $embed, $matches, $attr, $url, $rawattr );
+    return apply_filters( 'embed_zoom', $embed, $matches, $attr, $url, $rawattr );
 }
