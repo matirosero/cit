4f0dcaf00696338c68286b84de40c525952c4412
MatRosero <hello@matilderosero.com>
2019-07-12 23:21:05 -0600

    catchup

--- wp-content/themes/JointsCIT/functions/oembed.php
+++ wp-content/themes/JointsCIT/functions/oembed.php
@@ -18,9 +18,16 @@ add_action( 'init', function() {
 
 function wp_embed_handler_zoom( $matches, $attr, $url, $rawattr ) {
     $embed = sprintf(
-        '<div class="zoom-container"><iframe class="zoom-embed" src="%1$s" width="1200" height="675" frameborder="0" scrolling="no"></iframe></div>',
+        '<div class="zoom-container"><iframe class="zoom-embed" src="%1$s" width="640" height="360" frameborder="0" scrolling="no"></iframe></div>',
         esc_attr( $matches[1] ) 
      );
 
     return apply_filters( 'embed_zoom', $embed, $matches, $attr, $url, $rawattr );
 }
+
+
+function my_increase_oembed_timeout($args) {
+    $args["timeout"] = 30;
+    return $args;
+}
+add_filter( 'oembed_remote_get_args', 'my_increase_oembed_timeout');
