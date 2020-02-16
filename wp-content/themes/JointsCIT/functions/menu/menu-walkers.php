d0cd4fff89d346038281f44521246200de90e409
Matilde Rosero <mrosero@gmail.com>
2020-02-15 23:13:59 -0600

    medium dropdown and topbar login: initial and style

--- wp-content/themes/JointsCIT/functions/menu/menu-walkers.php
+++ wp-content/themes/JointsCIT/functions/menu/menu-walkers.php
@@ -1,6 +1,71 @@
 <?php
 
 class Topbar_Menu_Walker extends Walker_Nav_Menu {
+
+    function start_el(&$output, $item, $depth=0, $args=array(), $id = 0) {
+
+        $object = $item->object;
+        $type = $item->type;
+        $title = $item->title;
+        $description = $item->description;
+        $permalink = $item->url;
+
+        $classes = $item->classes;
+
+        $menu_item_id = $item->object_id;
+
+
+
+        
+
+        if ( in_array('menu-item-login', $classes) ) {
+
+            $output .= '<li id="'.$item->ID.'" class="' . implode(" ", $classes) . ' menu-item-has-children is-dropdown-submenu-parent opens-left" role="menuitem" aria-haspopup="true" aria-label="Login" data-is-click="false">';
+
+            $output .= '<a href="#">';
+            $output .= 'login';
+            $output .= '</a>';
+
+
+            $output .= '<ul class="menu submenu is-dropdown-submenu first-sub vertical" data-submenu="" role="menubar" style="">';
+            $output .= '<li id="2163" class="is-submenu-item is-dropdown-submenu-item menu-item-not-link" role="menuitem">';
+            $output .= '<div class="login">'.do_shortcode( '[login_form]' ).'</div>';
+            $output .= '</li>';
+            $output .= '</ul>';
+
+        } else {
+
+            $output .= '<li id="'.$item->ID.'" class="' . implode(" ", $classes) . '">';
+
+
+
+            if ( in_array('menu-item-logout', $classes) ) {
+
+                $output .= '<a href="' . wp_logout_url() . '">';
+
+            } else {
+                
+                // Check if link should open in new window
+                if ( $item->target == "_blank" ) {
+                    $output .= '<a href="' . $permalink . '" target="_blank">';
+                } else {
+                    $output .= '<a href="' . $permalink . '">';
+                }                
+            }
+
+
+
+            $output .= $title;
+
+            $output .= '</a>';
+
+        }
+
+
+
+    }
+
+
     function start_lvl(&$output, $depth = 0, $args = Array() ) {
         $indent = str_repeat("\t", $depth);
         $output .= "\n$indent<ul class=\"menu\">\n";
