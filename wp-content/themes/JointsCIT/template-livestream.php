df002d442bd64bb8da07e5b948764d713f58f56e
Matilde Rosero <mrosero@gmail.com>
2020-05-26 16:54:34 -0600

    livestream template

---
+++ wp-content/themes/JointsCIT/template-livestream.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Template Name: Livestream
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="container" class="expand medium-collapse ">
+
+	<?php get_template_part( 'parts/nav/sidebar' ); ?>
+
+	<div id="content" class="">
+
+		<div id="inner-content" class="grid-x grid-padding-x" data-equalizer="main-side" data-equalize-on="large">
+
+			<main id="main-livestream" class="small-12 cell" role="main" data-equalizer-watch="main-side" >
+
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+			    	<?php get_template_part( 'parts/loop/page' ); ?>
+
+			    <?php endwhile; endif; ?>
+
+
+			</main> <!-- end #main -->
+
+		    <?php //get_sidebar(); ?>
+
+		</div> <!-- end #inner-content -->
+
+	</div> <!-- end #content -->
+
+</div><!-- end #container -->
+
+<?php get_footer(); ?>
