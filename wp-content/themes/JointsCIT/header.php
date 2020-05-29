2b3823926d8e70003a7808b5b2524dcd8778aaed
Matilde Rosero <mrosero@gmail.com>
2020-05-29 07:47:25 -0600

    google tag manager after body

--- wp-content/themes/JointsCIT/header.php
+++ wp-content/themes/JointsCIT/header.php
@@ -38,6 +38,11 @@
 
 	<body <?php body_class(); ?>>
 
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W735CZ9"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
+
 		<div class="off-canvas-wrapper">
 
 			<?php get_template_part( 'parts/content/offcanvas' ); ?>
