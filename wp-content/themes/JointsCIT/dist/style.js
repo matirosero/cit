ac3d5aced1811aaced47a05355ac826326325e9f
Mat Rosero <mrosero@gmail.com>
2022-04-28 11:52:35 -0600

    webpack works

--- wp-content/themes/JointsCIT/dist/style.js
+++ wp-content/themes/JointsCIT/dist/style.js
@@ -1,25 +1,9 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./scss/style.scss":
+var __webpack_exports__ = {};
 /*!*************************!*\
   !*** ./scss/style.scss ***!
   \*************************/
-/***/ (() => {
-
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/css-loader/dist/cjs.js):\nError: Can't resolve '../fonts/fontello.eot?77867047' in '/Users/mati/Local Sites/cit/app/public/wp-content/themes/JointsCIT/scss'\n    at finishWithoutResolve (/Users/mati/Local Sites/cit/app/public/wp-content/themes/JointsCIT/node_modules/enhanced-resolve/lib/Resolver.js:309:18)\n    at /Users/mati/Local Sites/cit/app/public/wp-content/themes/JointsCIT/node_modules/enhanced-resolve/lib/Resolver.js:386:15\n    at /Users/mati/Local Sites/cit/app/public/wp-content/themes/JointsCIT/node_modules/enhanced-resolve/lib/Resolver.js:435:5\n    at eval (eval at create (/Users/mati/Local Sites/cit/app/public/wp-content/themes/JointsCIT/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:16:1)\n    at /Users/mati/Local Sites/cit/app/public/wp-content/themes/JointsCIT/node_modules/enhanced-resolve/lib/Resolver.js:435:5\n    at eval (eval at create (/Users/mati/Local Sites/cit/app/public/wp-content/themes/JointsCIT/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:27:1)\n    at /Users/mati/Local Sites/cit/app/public/wp-content/themes/JointsCIT/node_modules/enhanced-resolve/lib/DescriptionFilePlugin.js:87:43\n    at /Users/mati/Local Sites/cit/app/public/wp-content/themes/JointsCIT/node_modules/enhanced-resolve/lib/Resolver.js:435:5\n    at eval (eval at create (/Users/mati/Local Sites/cit/app/public/wp-content/themes/JointsCIT/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\n    at /Users/mati/Local Sites/cit/app/public/wp-content/themes/JointsCIT/node_modules/enhanced-resolve/lib/Resolver.js:435:5\n    at processResult (/Users/mati/Local Sites/cit/app/public/wp-content/themes/JointsCIT/node_modules/webpack/lib/NormalModule.js:758:19)\n    at /Users/mati/Local Sites/cit/app/public/wp-content/themes/JointsCIT/node_modules/webpack/lib/NormalModule.js:860:5\n    at /Users/mati/Local Sites/cit/app/public/wp-content/themes/JointsCIT/node_modules/loader-runner/lib/LoaderRunner.js:400:11\n    at /Users/mati/Local Sites/cit/app/public/wp-content/themes/JointsCIT/node_modules/loader-runner/lib/LoaderRunner.js:252:18\n    at context.callback (/Users/mati/Local Sites/cit/app/public/wp-content/themes/JointsCIT/node_modules/loader-runner/lib/LoaderRunner.js:124:13)\n    at Object.loader (/Users/mati/Local Sites/cit/app/public/wp-content/themes/JointsCIT/node_modules/css-loader/dist/index.js:154:5)\n    at processTicksAndRejections (node:internal/process/task_queues:96:5)");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./scss/style.scss"]();
-/******/ 	
+// extracted by mini-css-extract-plugin
 /******/ })()
 ;
 //# sourceMappingURL=style.js.map
